61d9fadd9b043aa5d6e2c0b35b801afe8ca86406
AlexWills37 <alexanderwills37@gmail.com>
2023-04-04 10:49:47 -0400

    moving sprite

--- test.cpp
+++ test.cpp
@@ -6,6 +6,7 @@
 
 int main()
 {
+    float movementSpeed = 2;
     std::cout << "! Starting program..." << std::endl;
 
     // Open Window
@@ -16,6 +17,8 @@ int main()
     sf::Texture wizard_texture;
     wizard_texture.loadFromFile("./assets/sprites/WizardSprite.png");
     sf::Sprite wizard(wizard_texture);
+    wizard.setPosition(300, 500);
+    wizard.setOrigin(38, 132);
 
 
     // Set up text
@@ -23,13 +26,24 @@ int main()
     font.loadFromFile("./assets/fonts/ShadowsIntoLight-Regular.ttf");
     sf::Text text;
     text.setFont(font);
-    text.setPosition(window.getSize().x / 2 - 100, 0);
+    text.setPosition(window.getSize().x / 2 - 140, 0);
     text.setCharacterSize(60);
-    text.setFillColor(sf::Color::Blue);
+    text.setFillColor(sf::Color(0, 200, 123));
     text.setString("Testing");
+
+    // Load music
+    sf::Music music;
+    if( !music.openFromFile("./assets/music/hollow_knight.ogg")){
+        std::cout<<"ERROR"<<std::endl;
+    }
+    music.play();
+
 
 
     window.draw(wizard);
+
+    bool movingLeft = false;
+    bool movingRight = false;
 
     // Keep window open
     while (window.isOpen())
@@ -37,9 +51,43 @@ int main()
         sf::Event event;
         while (window.pollEvent(event))
         {
+            // Register if the window is closed
             if (event.type == sf::Event::Closed) {
                 window.close();
             }
+                // Register if a key is pressed
+            else if (event.type == sf::Event::KeyPressed) {
+                if (event.key.code == sf::Keyboard::A) {
+                    movingLeft = true;
+                }
+                else if (event.key.code == sf::Keyboard::D) {
+                    movingRight = true;
+                }
+            }
+                // Register if a key is released
+            else if (event.type == sf::Event::KeyReleased) {
+                if (event.key.code == sf::Keyboard::A) {
+                    movingLeft = false;
+                }
+                else if (event.key.code == sf::Keyboard::D) {
+                    movingRight = false;
+                }
+            }
+
+        }   // Input has been handled
+
+        // Move the sprite
+        if (movingLeft && !movingRight) {
+            wizard.move(-movementSpeed, 0);
+            wizard.setScale(-1, 1);
+            text.setString("Moving Left!");
+        } else if (movingRight && !movingLeft) {
+            wizard.move(movementSpeed, 0);
+            wizard.setScale(1, 1);
+            text.setString("Moving Right!");
+        }
+        else {
+            text.setString("Not Moving!");
         }
 
 
